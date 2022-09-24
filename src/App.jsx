adb32a6180c0c4557f1f13dd6920af089d9459df
hugoorlando <hugo.orlando2030@gmail.com>
2022-09-23 22:16:08 -0300

    preview article

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
 import { useState } from 'react';
-
-import './App.css';
+import { Share } from './Share';
 
 function App() {
-  return <h1>preview article</h1>;
+  const [isActive, setIsActive] = useState(true);
+
+  return (
+    <main>
+      <div class="image-container"></div>
+      <section>
+        <div class="copy">
+          <h2>
+            Shift the overall look and feel by adding these wonderful touches to
+            furniture in your home
+          </h2>
+          <p>
+            Ever been in a room and felt like something was missing? Perhaps it
+            felt slightly bare and uninviting. I’ve got some simple tips to help
+            you make any room feel complete.
+          </p>
+        </div>
+        <footer>
+          <div class="author">
+            <img
+              src="../src/assets/images/avatar-michelle.jpg"
+              alt="michelle appleton avatar"
+            />
+            <div class="about">
+              <h3>Michelle Appleton</h3>
+              <time datetime="2020-06-07">28 Jun 2020</time>
+            </div>
+          </div>
+          <div onClick={() => setIsActive(!isActive)} class="arrow">
+            <img src="../src/assets/images/icon-share.svg" alt="share" />
+          </div>
+          {isActive && <Share />}
+        </footer>
+      </section>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/Share.jsx
@@ -0,0 +1,33 @@
+export const Share = () => {
+  return (
+    <div class="social-media">
+      <p>share</p>
+      <ul>
+        <li>
+          <a href="#">
+            <img
+              src="../src/assets/images/icon-facebook.svg"
+              alt="facebook icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img
+              src="../src/assets/images/icon-twitter.svg"
+              alt="facebook icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img
+              src="../src/assets/images/icon-pinterest.svg"
+              alt="facebook icon"
+            />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
