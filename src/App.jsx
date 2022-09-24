f1a6113f46653f70c30dc20639eb57cd0669033b
hugoorlando <hugo.orlando2030@gmail.com>
2022-09-23 22:29:21 -0300

    preview article

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
         <footer>
           <div class="author">
             <img
-              src="./assets/images/drawers.jpg"
+              src="../src/assets/images/drawers.jpg"
               alt="michelle appleton avatar"
             />
             <div class="about">
@@ -31,7 +31,7 @@ function App() {
             </div>
           </div>
           <div onClick={() => setIsActive(!isActive)} class="arrow">
-            <img src="/src/images/icon-share.svg" alt="share" />
+            <img src="../src/assets/images/icon-share.svg" alt="share" />
           </div>
           {isActive && <Share />}
         </footer>
